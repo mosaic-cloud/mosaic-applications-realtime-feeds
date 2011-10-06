cb84ab4b3e45c1d9292344d2e12a929ccce7ae39
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-10-06 12:26:47 +0300

    Minor NPM fixup.

--- backend/scripts/npm.bash
+++ backend/scripts/npm.bash
@@ -4,6 +4,10 @@ if test "${#}" -ge 1 ; then
 	_npm_args+=( "${@}" )
 fi
 
+if ! test -e "${_npm_prefix}" ; then
+	mkdir "${_npm_prefix}"
+fi
+
 if test "${#_npm_args[@]}" -eq 0 ; then
 	exec env "${_npm_env[@]}" "${_npm}"
 else
