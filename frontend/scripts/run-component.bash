55d1c66dfe03aaf115997f094fd6ab4e8c18d4fd
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-03-08 18:23:42 +0200

    Update POM and scripts in line with the rest of the Java platform

--- frontend/scripts/run-component.bash
+++ frontend/scripts/run-component.bash
@@ -1,18 +1,30 @@
 #!/dev/null
 
-if ! test "${#}" -le 1 ; then
-	echo "[ee] invalid arguments; aborting!" >&2
-	exit 1
-fi
-
 _identifier="${1:-00000000cf14614e8810102fa887b6bc90dc2a40}"
+if test "${#}" -ge 1 ; then
+	for _argument in "${@}" ; do
+		if test -z "${_identifier}" ; then
+			_identifier="${_argument}"
+			break
+		fi
+		case "${_argument}" in
+			( --component-identifier )
+				_identifier=''
+			;;
+		esac
+	done
+fi
 
 if test -n "${mosaic_component_temporary:-}" ; then
 	_tmp="${mosaic_component_temporary:-}"
-elif test -n "${mosaic_node_temporary:-}" ; then
-	_tmp="${mosaic_node_temporary}/components/${_identifier}"
+elif test -n "${mosaic_temporary:-}" ; then
+	_tmp="${mosaic_temporary}/components/${_identifier}"
 else
 	_tmp="/tmp/mosaic/components/${_identifier}"
+fi
+
+if test -n "${mosaic_component_debug:-}" ; then
+	_java_args+=( -Dlogback.levels.root=debug )
 fi
 
 _jar="${_java_jars:-${_workbench}/target}/${_package_jar_name}"
@@ -20,12 +32,11 @@ _war="${_java_jars:-${_workbench}/target}/${_package_war_name}"
 
 _java_args+=(
 		-jar "${_jar}"
-		"${_identifier}"
 		"${_war}"
+		"${@}"
 )
 
 mkdir -p -- "${_tmp}"
 cd -- "${_tmp}"
 
 exec env "${_java_env[@]}" "${_java_bin}" "${_java_args[@]}"
-exit 1
