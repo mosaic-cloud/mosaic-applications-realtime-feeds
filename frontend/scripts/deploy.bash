a85d275411f00a77209dcc36a080c90ca006548a
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-05-17 19:01:57 +0300

    Update deploy script to push Maven artifacts

--- frontend/scripts/deploy.bash
+++ frontend/scripts/deploy.bash
@@ -9,4 +9,14 @@ if test "${_mosaic_deploy_cook:-true}" == true ; then
 	ssh -T "${_package_cook}" <"${_outputs}/package.tar.gz"
 fi
 
+if test "${_mosaic_deploy_artifactory:-true}" == true ; then
+	env "${_mvn_env[@]}" "${_mvn_bin}" \
+			-f "${_mvn_pom}" \
+			--projects "${_maven_pom_group}:${_maven_pom_artifact}" \
+			--also-make \
+			"${_mvn_args[@]}" \
+			deploy \
+			-DskipTests=true
+fi
+
 exit 0
