d81f9aa1bd74751b38a17154b633db6023319de1
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-07-13 14:11:43 +0300

    Move forward the package version

--- frontend/scripts/_env.bash
+++ frontend/scripts/_env.bash
@@ -61,5 +61,5 @@ test -n "${_maven_pom_package}"
 _package_name="${_maven_pom_package}"
 _package_jar_name="${_maven_pom_artifact}-${_maven_pom_version}-${_maven_pom_classifier}.jar"
 _package_scripts=( run-component )
-_package_version="${mosaic_distribution_version:-0.2.0_mosaic_dev}"
+_package_version="${mosaic_distribution_version:-0.2.1_mosaic_dev}"
 _package_cook="${mosaic_distribution_cook:-cook@agent1.builder.mosaic.ieat.ro}"
