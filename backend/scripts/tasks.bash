5596b45a982ef239a5552c257716f8bfed20b9eb
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-08 13:56:36 +0300

    Update tasks

--- backend/scripts/tasks.bash
+++ backend/scripts/tasks.bash
@@ -7,7 +7,9 @@ fi
 
 cat <<EOS
 
-${_package_name}@requisites : pallur-packages@nodejs pallur-bootstrap
+${_package_name}@requisites : \
+		pallur-packages@nodejs \
+		pallur-environment
 
 ${_package_name}@prepare : ${_package_name}@requisites
 	!exec ${_scripts}/prepare
@@ -20,12 +22,6 @@ ${_package_name}@compile : ${_package_name}@prepare
 
 ${_package_name}@deploy : ${_package_name}@package
 	!exec ${_scripts}/deploy
-
-pallur-distribution@requisites : ${_package_name}@requisites
-pallur-distribution@prepare : ${_package_name}@prepare
-pallur-distribution@compile : ${_package_name}@compile
-pallur-distribution@package : ${_package_name}@package
-pallur-distribution@deploy : ${_package_name}@deploy
 
 EOS
 
