1d4c505fbe027ebe7adb89e93e7e05fdaf3d29d5
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-10-29 17:46:10 +0200

    Update publish script

--- backend/scripts/publish.bash
+++ backend/scripts/publish.bash
@@ -9,8 +9,11 @@ fi
 ## chunk::d63e3622fd5d8efb682c6cb0faffe808::begin ##
 if test "${pallur_publish_cp:-false}" == true ; then
 	test -n "${pallur_publish_cp_store}"
-	pallur_publish_cp_target="${pallur_publish_cp_store}/${_package_name}--${_package_version}.cpio.gz"
+	pallur_publish_cp_target="${pallur_publish_cp_store}/${_package_name}/${_package_version}/package.cpio.gz"
 	echo "[ii] publishing via \`cp\` method to \`${pallur_publish_cp_target}\`..." >&2
+	if test ! -e "$( dirname -- "${pallur_publish_cp_target}" )" ; then
+		mkdir -p -- "$( dirname -- "${pallur_publish_cp_target}" )"
+	fi
 	cp -T -- "${_outputs}/package.cpio.gz" "${pallur_publish_cp_target}"
 fi
 ## chunk::d63e3622fd5d8efb682c6cb0faffe808::end ##
@@ -19,7 +22,7 @@ fi
 if test "${pallur_publish_curl:-false}" == true ; then
 	test -n "${pallur_publish_curl_credentials}"
 	test -n "${pallur_publish_curl_store}"
-	pallur_publish_curl_target="${pallur_publish_curl_store}/${_package_name}--${_package_version}.cpio.gz"
+	pallur_publish_curl_target="${pallur_publish_curl_store}/${_package_name}/${_package_version}/package.cpio.gz"
 	echo "[ii] publishing via \`curl\` method to \`${pallur_publish_curl_target}\`..." >&2
 	env -i "${_curl_env[@]}" "${_curl_bin}" "${_curl_args[@]}" \
 			--anyauth --user "${pallur_publish_curl_credentials}" \
